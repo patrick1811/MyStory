b76ca875ab1edff7a03e12d313ce2b0da2d8ca7a
Patrick <bakerpatrick644@yahoo.com>
2023-09-04 10:43:33 -0400

    Initial Commit

---
+++ MyStory/MyStoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyStoryApp.swift
+//  MyStory
+//
+//  Created by Patrick Baker on 9/4/23.
+//
+
+import SwiftUI
+
+@main
+struct MyStoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
